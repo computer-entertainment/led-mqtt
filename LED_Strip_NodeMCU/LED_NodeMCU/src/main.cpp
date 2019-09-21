81d684ec4d3455df610c802422c8d3447777e75c
yolomilk <44xy2013@gmail.com>
2019-09-21 10:42:05 +0200

    Squashed commit of the following:
    commit c713c953e03773aa616539074ded592ae9b06b1f
    Author: yolomilk <44xy2013@gmail.com>
    Date:   Sat Sep 21 10:37:15 2019 +0200
    
        contract für das ledstate struct hinzugefügt.
    
    commit 378d50511e8e3cd933a70591a31c1d3de2a619bb
    Author: yolomilk <44xy2013@gmail.com>
    Date:   Fri Sep 20 22:47:36 2019 +0200
    
        Fetures: aus, Regenbogen, Rot

--- LED_Strip_NodeMCU/LED_NodeMCU/src/main.cpp
+++ LED_Strip_NodeMCU/LED_NodeMCU/src/main.cpp
@@ -1,9 +1,103 @@
 #include <Arduino.h>
+#include <FastLED.h>
+#include "../../../contracts/basetypes.h"
 
-void setup() {
-    // put your setup code here, to run once:
+#define LED_PIN 3
+
+// Information about the LED strip itself
+
+#define NUM_LEDS 60
+
+#define CHIPSET WS2811
+
+#define COLOR_ORDER GRB
+
+CRGB leds[NUM_LEDS];
+
+//current Animation variables:
+uint32_t currentFrame = 0;
+uint16_t currentSpeedVal = 0;
+
+// Animations Variablen, die über MQTT aktuallisiert werden:
+struct ledState myLedState = {(uint8_t)128, (uint16_t)1500, (uint8_t)0, (uint16_t)0, (uint8_t)2, (uint8_t)0, (uint32_t)0xAA0000};
+
+void fillColor(CRGB color, uint8_t from = 0, uint8_t to = NUM_LEDS)
+{
+    for (int i = from; i < to; i++)
+    {
+        leds[i] = color;
+    }
 }
 
-void loop() {
+void off()
+{
+    fillColor(CRGB::Black);
+}
+
+void rainbow(uint32_t Frame, uint8_t from = 0, uint8_t to = NUM_LEDS, uint8_t deltahue = 5, uint8_t initialhue = 0)
+{
+    fill_rainbow(&leds[from], to - from, (initialhue + Frame * deltahue) % 255, deltahue);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+
+    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalSMD5050);
+
+    FastLED.setBrightness(myLedState.brightness);
+    fillColor(CRGB(myLedState.color));
+    FastLED.show();
+    delay(1000);
+}
+
+void loop()
+{
     // put your main code here, to run repeatedly:
+
+    if (myLedState.speed < currentSpeedVal)
+    {
+        currentSpeedVal = 0;
+
+        switch (myLedState.currentAnimationId)
+        {
+        case 0:
+            off();
+            break;
+        case 1:
+            fillColor(CRGB(myLedState.color));
+            FastLED.show();
+            break;
+        case 2:
+            rainbow(currentFrame);
+            currentFrame++;
+            FastLED.show();
+            break;
+        }
+    }
+    else
+    {
+        currentSpeedVal++;
+    }
+}
+
+void MQTTCallback(char *topic, byte *payload, unsigned int length)
+{
+}
+
+void serialEvent()
+{
+    while (Serial.available())
+    {
+        // get the new byte:
+        char inChar = (char)Serial.read();
+        // add it to the inputString:
+        //inputString += inChar;
+        // if the incoming character is a newline, set a flag so the main loop can
+        // do something about it:
+        if (inChar == '\n')
+        {
+            //stringComplete = true;
+        }
+    }
 }
---
+++ contracts/basetypes.h
@@ -0,0 +1,12 @@
+#pragma once
+
+struct ledState{
+uint8_t brightness;
+uint16_t speed;
+uint8_t fadeId;
+uint16_t fadeSpeed;
+uint8_t currentAnimationId;
+uint8_t nextAnimationId;
+uint32_t color;
+
+};
